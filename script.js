bbad2859e9e8570fb5a5f7dad4609687dd534767
Imankkkk <imandanishzaini2008@gmail.com>
2025-03-19 04:20:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+document.getElementById("next").addEventListener("click", () => {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    slides[currentSlide].classList.add("active");
+});
